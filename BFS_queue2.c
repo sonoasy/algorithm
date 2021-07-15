4bfac1242c8a6dbbeed7ead9a46310296f184133
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-07-15 23:25:31 +0900

    Update BFS_queue2.c

--- BFS_queue2.c
+++ BFS_queue2.c
@@ -99,6 +99,29 @@ void bfs(Graph *g,int v){
   }
 }
     
+int main(){
+ Graph *g;
+ g=(Graph *)malloc(sizeof(Graph));
+ graph_init(g);
+ for(int i=0;i<6;i++)
+  insert_vertext(g,i);
+ insert_edge(g,0,2);
+ insert_edge(g,2,1);
+ insert_edge(g,2,3);
+ insert_edge(g,0,4);
+ insert_edge(g,4,5);
+ insert_edge(g,1,5);
+ 
+ BFS(g,0);
+ printf("\n");
+ free(g);       
+ 
+ 
+ 
+ 
+ 
+ 
+}
     
   
 
