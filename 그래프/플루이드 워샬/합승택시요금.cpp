147c78514ea6bb99ee14b9e30e51a859ec479020
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2022-03-14 23:36:22 +0900

    --ing

---
+++ 그래프/플루이드 워샬/합승택시요금.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = 0;
+    vector<vector<int>> f;
+    f.assign(n+1,vector<int>(n+1,0));
+    //fares 초기화 -> 대칭으로 반대편도 해줘야함(플루이드 워샬 양방향이므로), 자기자신은 0으로 해야함
+   for(int i=1;i<=n;i++){
+       f[i][i]=0;
+        
+   }
+        
+    for(int i=0;i<fares.size();i++){
+        int ss=fares[i][0];
+        int e=fares[i][1];
+        int costs=fares[i][2];
+        
+        f[ss][e]=costs;
+        f[e][ss]=costs;
+    }
+    
+    vector<vector<int>> res;
+    f.assign(n+1,vector<int>(n+1,200002)); //100002-> 최대 100000인데 n-1 지날수 있으므로 2배정도로 하기
+    //플루이드 워샬문제 3중 루프까진 기억나는데 범위..? -> 제일 바깥 루프가 중간임
+    
+    //요금 저장하는 벡터 따로, 결과 저장하는 벡터 따로..? 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n;k++){
+                
+                res[j][k]=min(res[j][k],f[j][i]+f[i][k]);
+            }
+            
+        }
+    }
+    
+    //s->k->a , s->k->b 가 있으면 s->a + s->b보다 작은지 비교해서 값 갱신하기 
+    answer=res[s][a]+res[s][b];
+    for(int k=1;k<=n;k++){
+        //없으면 min에서 걸러지지 않나
+        answer=min(answer,res[s][k]+res[k][a]+res[k][b]);        
+    }
+    //illegal instruction (core dumped) 오류 뜸 
+    
+    
+    
+    return answer;
+}
