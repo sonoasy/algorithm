35f1188ffbb9a4b14201625e966e25b157e25293
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2022-08-23 23:46:15 +0900

    다익스트라 구현 O(N^2) bfs랑 비슷

---
+++ 최단경로/다익스트라/다익스트라.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <queue>
+
+//아직 방문하지 않은 연결된 노드 중에 가장 거리가 짧은 노드 선택하기 
+//지금 위치에서 가장 짧으면 최단거리임! 
+int Find_Shortest_Node()
+{
+	int min_dist = INF;
+    int min_idx = -1;
+    for (int i = 0; i<= N; i++)
+    {
+    	if (visited[i] == true)
+        	continue;
+        if (dist[i] < min_dist)
+        {
+        	min_dist = dist[i];
+            min_idx = i;
+        }
+    }
+    return min_idx;    
+}
+
+
+void Dijkstra()
+{
+	for (int i = 1; i <= N; i++)
+    	dist[i] = map[start][i];
+    
+    dist[start] = 0;
+    visited[start] = true;
+    
+    for (int i = 0; i < N - 1; i++)
+    {
+    	int new_node = Find_Shortest_Node();
+        visited[new_node] = true;
+        
+        for(int j = 0; j <= N; j++)
+        {
+        	if (visited[j] == true)
+            	continue;
+            if (dist[j] > dist[new_node] + map[new_node][j])
+            	dist[j] = dist[new_node] + map[new_node][j];
+        }
+    }
+}
